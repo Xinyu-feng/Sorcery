9f68e03dc135b39a712e0673b713e655aab12912
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-28 01:57:36 -0500

    Add new file

---
+++ enchantment.h
@@ -0,0 +1,16 @@
+#ifndef _ENCHANTMENT_H_
+#define _ENCHANTMENT_H_
+
+#include "card.h"
+
+class Enchantment: public Card{
+    int attackBoost = 0;
+    int defenseBoost = 0;
+    int attackMultiplier = 0;
+    int defenseMultiplier = 0;
+    
+public:
+    Enchantment(std::string name, std::shared_ptr<Player> owner);
+    
+    void runEffect(State s);
+}
