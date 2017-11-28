a541109d1a272a3ea2764bd4ee65e13c67702835
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-28 03:52:10 -0500

    Update enchant.cc

--- enchant.cc
+++ enchant.cc
@@ -1,6 +1,7 @@
 #include "enchant.h"
 
 #include <utility>
+#include "enchantment.h"
 
 Enchant::Enchant(std::shared_ptr<Minion> m, std::shared_ptr<Enchantment> e):MinionDecorator{m}, e{e}{
     if (e.getName() == "Giant Strength"){
@@ -29,5 +30,19 @@ void Enchant::runEffect(Subject &b){
         }
     }
     
+}
+
+std::shared_ptr<Enchantment> Enchant::removeEnchant(){
+    if (e.getName() == "Giant Strength"){
+        m->lowerStats(2, 2);
+    }
+    else if (e.getName() == "Enrage"){
+        m->setStats(m.getAttack() / 2, m.getDefense() / 2);
+    }
+    else if (e.getName() == "Magic Fatigue"){
+        m->lowerAbilityCost(2);
+    }
+    
+    return e;
 }
     
