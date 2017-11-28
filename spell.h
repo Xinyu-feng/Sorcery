dd525803d8464abe2a350d7344a61d02e25c77c7
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-27 23:47:40 -0500

    Add new file

---
+++ spell.h
@@ -0,0 +1,16 @@
+#ifndef _SPELL_H_
+#define _SPELL_H_
+
+#include "card.h"
+
+class Spell: public Card{
+    
+public:
+    Spell(std::string name, std::shared_ptr<Player> owner);
+    
+    void runEffect(Subject s);
+    
+    void displayCard();
+}
+
+#endif
