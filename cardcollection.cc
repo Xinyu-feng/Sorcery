6467f882f7c92cb30c7e750216d03b35b5d33462
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-29 18:19:45 -0500

    Update cardcollection.cc

--- cardcollection.cc
+++ cardcollection.cc
@@ -1,7 +1,21 @@
 #include "cardcollection.h"
 
-void CardCollection::moveCard(int i, CardCollection &collect) {
-	collect.cardList.emplace_back(cardList.at(i));
+void CardCollection::moveCard(CardCollection &collect, int cardPosition, bool board, int target) {
+    
+    int position = cardPosition;
+    
+    if (cardPosition == -1){
+        position = getCardCount() - 1;
+    }
+    
+    if (board){
+        playCard(cardList.at(position), target);
+    }
+    else{
+        collect.cardList.emplace_back(cardList.at(position));
+    }
+	cardList.erase(cardList.begin() + position);
+    }
 }
 
 int CardCollection::getCardCount(){
