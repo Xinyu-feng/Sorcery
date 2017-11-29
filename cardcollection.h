0c38bfd093c9c6eb5109b844745f5c17ebd10beb
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-29 18:03:46 -0500

    Update cardcollection.h

--- cardcollection.h
+++ cardcollection.h
@@ -6,7 +6,9 @@ class Card;
 class CardCollection {
 	std::vector <std::shared_ptr<Card>> cardList;
 public:
-	void moveCard(int i, CardCollection &collect);
+    // -1 indicates you want to move the top card
+    
+	void moveCard(CardCollection &collect, int cardPosition = -1, bool board = false);
 	int getCardCount();
 	virtual ~CardCollection() = 0;
 }
