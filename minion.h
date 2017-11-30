a46889daa74038784586d65d38c4a06850045002
Xinyu Feng <x55feng@uwaterloo.ca>
2017-11-30 15:31:00 -0500

    Update minion.h

--- minion.h
+++ minion.h
@@ -3,24 +3,21 @@
 
 #include "card.h"
 
-class Minion: public Card{
+class Minion: public Card {
     
     int attack;
     int defense;
-    int actions = 0;
-    int activateCost = 0;
-    
+    int maxActions;
+    int currActions = 0;
+
 public:
 
-    Minion(std::string name, std::shared_ptr<Player> owner);
+    Minion(std::string name, std::shared_ptr<Player> owner, int manaCost, int attack, int defence, int maxActions = 1, string description = "");
 
     void addAction(int i);
     void lowerAction(int i);
     void setAction(int i);
     int getAction();
-    
-    void addAttack(int i);
-    void addDefense(int i);
     
     void addStats(int attack, int defence);
     
@@ -28,22 +25,11 @@ public:
     void setDefense(int i);
     
     void setStats(int attack, int defence);
-    
-    void lowerAttack(int i);
-    void lowerDefense(int i);
-    
-    void lowerStats(int attack, int defense);
-    
+  
     int getAttack();
     int getDefense();
     
-    void addActivateCost(int i);
-    void setActivateCost(int i);
-    void lowerActivateCost(int i);
-    int getActivateCost();
-    
-    
-    virtual void runEffect(State s)=0;
+    virtual void runEffect(State s) = 0;
     
     virtual void ~Minion() = 0;
 };
