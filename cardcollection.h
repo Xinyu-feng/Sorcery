4266eb9f212fa02a840276944483b3ad1cdf832a
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-29 23:12:54 -0500

    Update cardcollection.h

--- cardcollection.h
+++ cardcollection.h
@@ -1,3 +1,7 @@
+#ifndef _CARD_COLLECTION_H_
+#define _CARD_COLLECTION_H_
+
+
 #include <memory>
 #include <vector>
 
@@ -7,12 +11,20 @@ class CardCollection {
 	std::vector <std::shared_ptr<Card>> cardList;
 public:
 
+
     // This function moves a card to the collect CardCollection
     // cardPosition represents the position of the card in the deck
     // The board flag indicates that you are moving the card to the board which requires special behaviour
     // In the case that the board is the destination, you need to track the target for spells and enhancements
 	void moveCard(CardCollection &collect, int cardPosition = -1, bool board = false, int target = -1);
 	virtual void addCard(std::shared_ptr<Card> card);
+	
+	void setCard(std::shared_ptr<Card> card, int index);
+	void deleteCard(int index);
+	
 	int getCardCount();
 	virtual ~CardCollection() = 0;
-}
+};
+
+
+#endif
