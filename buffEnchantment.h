bdbe5bde49801c198f4d7867e244fd1233a4a960
Xinyu Feng <x55feng@uwaterloo.ca>
2017-11-30 15:53:06 -0500

    Update buffEnchantment.h

--- buffEnchantment.h
+++ buffEnchantment.h
@@ -5,3 +5,5 @@ class BuffEnchantment : public Enchantment {
     public:
         BuffEnchantment(std::string name, std::shared_ptr<Player> owner, int manaCost, std::string attBuff, std::string defBuff, std:string description = "");
 };
+
+#endif
