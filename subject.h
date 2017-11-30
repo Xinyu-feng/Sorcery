c694c89ce659f8198538aacb45206445dcb46924
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-30 17:39:04 -0500

    Update subject.h

--- subject.h
+++ subject.h
@@ -1,13 +1,15 @@
 #ifndef SUBJECT_H
 #define SUBJECT_H
 #include <vector>
+#include <memory>
 
 class Observer;
 
 class Subject {
-	std::vector<Observer*> observers;
-	public:
-	void attach(Observer *o);
+	std::vector<std::shared_ptr<Observer>> observers;
+public:
+	void attach(std::shared_ptr<Observer>, int insert = -1);
+	void detach(std::shared_ptr<Observer>);
 	void notifyObservers();
 };
 
