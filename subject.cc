107f38b14f137fc273dd507320afce251347109e
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-30 17:39:10 -0500

    Update subject.cc

--- subject.cc
+++ subject.cc
@@ -1,7 +1,22 @@
 #include "subject.h"
+#include "observer.h"
 
-void Subject::attach(Observer *o) {
-	observers.emplace_back(o);
+void Subject::attach(std::shared_ptr<Observer> o, int insert) {
+    if (insert == -1 || insert >= observers.size()){
+	    observers.emplace_back(o);
+    }
+    else{
+        observers.insert(observers.begin() + insert, o);
+    }
+}
+
+void Subject::detach(std::shared_ptr<Observer> o){
+    for (auto it = observers.begin(); it != observers.end(); ++it){
+        if (*it == o){
+            observers.erase(it);
+            break;
+        }
+    }
 }
 
 void Subject::notifyObservers() {
