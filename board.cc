d6f2b3ceff3be72e14fa58abd12959f994b5f1e4
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-30 20:24:48 -0500

    Update board.cc

--- board.cc
+++ board.cc
@@ -1,5 +1,20 @@
-#include "board.h"
+#include <board.h>
 
-Board::Board():CardCollection{}, minionCount{0}{
+Board::Board():CardCollection{}, BoardSubject{}, minionCount{0}{
     addCard(nullptr);
+}
+
+void Board::playMinion(std::shared_ptr<Minion> m){
+    attachMinion(m, true);
+    addCard(m);
+    minionCount += 1;
+}
+
+void Board::playEnchant(std::shared_ptr<Enchantment> e, int target){
+    
+}
+
+void Board::playRitual(std::shared_ptr<Ritual> r, int target){
+    attachRitual(r, true);
+    
 }
