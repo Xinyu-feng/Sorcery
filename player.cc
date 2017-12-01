40f8bb6a1a0e96aafe1ab4759bd4c35cedef22a9
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-12-01 02:42:33 -0500

    Add new file

---
+++ player.cc
@@ -0,0 +1,73 @@
+#include "player.h"
+
+
+
+Player::Player(int player, std::string name, std::string deckFile, bool shuffle, int life, int magic):player{player}, name{name}, deck{deckFile}, life{life}, magic{magic}{
+    if (shuffle) deck.shuffle();
+    
+    for (int i = 0; i < 5; ++i) draw();
+}
+
+
+void Player::draw(){
+    deck.moveCard(hand, deck);
+}
+
+void play(int i, int p, char t){
+    int target;
+    
+    // Index on board for ritual is located at 0
+    if (t == 'r'){
+        target = 0;
+    }
+    // If still default value, make sure it's -1
+    else if (t == '\0'){
+        target = -1;
+    }
+    // Convert char index into int from 1 to 5
+    else{
+        target = 'a' - t;
+    }
+    
+
+    if (p == 1){
+        moveCardToBoard(myBoard, i - 1, target);
+    }
+    else{
+        moveCardToBoard(*otherBoard, i - 1, target);
+    }
+}
+
+void attack(int i, int j = 0);
+void use(int i, int p, int t);
+//void displayBoard();
+	
+void Player::discard(int i){
+    hand.discard(i);
+}
+	void displayHand();
+	
+void Player::addMagic(int i){
+    magic += i;
+}
+
+void Player::deductMagic(int i){
+    magic -= i;
+    if (magic < 0) magic = 0;
+}
+
+int Player::getMagic(){
+    return magic;
+}
+	
+void Player::addLife(int i){
+    life += i;
+}
+void Player::deductLife(int i){
+    life -= i;
+    if (life < 0) life = 0;
+}
+
+int getLife(){
+    return life;
+}
