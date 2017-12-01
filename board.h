faab5f4c92d397b1b5338a8c734d1305364092ad
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-11-30 21:20:28 -0500

    Update board.h

--- board.h
+++ board.h
@@ -21,22 +21,20 @@ class Board: public CardCollection {
 	
 	int getMinionCount();
 	
-	/*
+	
 	void playMinion(std::shared_ptr<Minion> m);
 	void playEnchant(std::shared_ptr<Enchantment> e, int target);
 	void playRitual(std::shared_ptr<Ritual> r);
-	*/
-	void playCard(std::shared_ptr<Minion> card, int i);
-	void playCard(std::shared_ptr<Enchantment> card, int target);
-	void playCard(std::shared_ptr<Ritual> card, int i);
-	void playCard(std::shared_ptr<Spell> card, int target);
-	void use(int i, int t);
-	std::vector<string> inspect(int i);
-	//std::vector<string> displayBoard();
-	void attack(int i, int j);
-	void inflictDamage(int i, int d);
-	int getMinionCount();
-	Card removeEnchant(int i);
+    
+
+    void use(int i, int t);
+    std::vector<string> inspect(int i);
+    //std::vector<string> displayBoard();
+    void attack(int i, int j);
+    void inflictDamage(int i, int d);
+    int getMinionCount();
+    Card removeEnchant(int i);
+
 
 };
 
