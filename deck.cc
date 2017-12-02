0427566f85cf8f4a5ef251fa66694334b05223e1
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-02 03:03:16 -0500

    Update deck.cc

--- deck.cc
+++ deck.cc
@@ -1,84 +1,34 @@
 #include "deck.h"
-
 #include "card.h"
-#include "spell.h"
-#include "enchantment.h"
-#include "ritual.h"
-#include "basicminion.h"
+#include "helper.h"
 
 #include <string>
-#include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include <memory>
+#include <algorithm>
 
+using namespace std;
 
-const std::vector<std::string> minions = {"Air Elemental", "Earth Elemental", "Bone Golem", "Fire Elemental", "Potion Seller", "Novice Pyromancer", "Apprentice Summoner", "Master Summoner"};
-const std::vector<std::string> spells = {"Banish", "Unsummon", "Recharge", "Disenchant", "Raise Dead", "Blizzard"};
-const std::vector<std::string> enchantments = {"Giant Strength", "Enrage", "Haste", "Magic Fatigue", "Silence"};
-const std::vector<std::string> rituals = {"Dark Ritual", "Aura of Power", "Standstill"};
-
-
-
-bool inVector(std::vector<std::string> &v, std::string s){
-    int length = v.size();
-    for (int i = 0; i < length; ++i){
-        if (v.at(i) == s){
-            return true;
-        }
-    }
-    return false;
-}
-
-
-Deck::Deck(std::string deckFile, std::shared_ptr<Player> owner, bool shuffle){
-    std::ifstream deck{deckFile};
+Deck::Deck(string deckFile, shared_ptr<Player> owner){
+    ifstream deck{deckFile};
     
     deck.open();
     
-    std::string cardName;
+    string cardName;
     
     while(deck.getline(cardName)){
-        if (inVector(minions, cardName)){
-            addCard(std::shared_ptr<Basic_Minion>(new Basic_Minion{cardName, owner});
-        }
-        else if (inVector(spells, cardName)){
-            addCard(std::shared_ptr<Spell>(new Spell{cardName, owner});
-        }
-        else if (inVector(enchantments, cardName)){
-            addCard(std::shared_ptr<Enchantment>(new Enchantment{cardName, owner});
-        }
-        else if (inVector(rituals, cardName)){
-            addCard(std::shared_ptr<Ritual>(new Ritual{cardName, owner});
-        }
-        else{
-        }
-        
+        addCard(createCard(cardName));
     }
-    
-    if (shuffle){
-        shuffle();
-    }
+
+    shuffle();
 }
 
 void Deck::shuffle(){
-    std::vector<std::shared_ptr<Card>> tempHolder;
+    int len = getCardCount();
     
-    int originalLength = getCardCount();
-    int length = originalLength;
-    
-    while (length > 0){
-        int randIndex = rand() % length;
-        
-        tempHolder.emplace_back(getCard(randIndex));
-        deleteCard(randIndex);
-        
-        length -= 1;
+    // swaps two random cards in the vector len * 3 times
+    for (int i = 0; i < len * 3; ++i) {
+        int randIndex1 = rand() % len;
+        int randIndex2 = rand() % len;
+        iter_swap(cardList.begin() + randIndex1, cardList.begin() + randIndex2); 
     }
-    
-    for (int i = 0; i < originalLength; ++i){
-        addCard(tempHolder.at(i));
-    }
-   
-   // swap temp vector with original cards
 }
