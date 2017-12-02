0c34e3b0065f3f403e0d77d5d803789dbc508fc5
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-02 02:20:32 -0500

    Update deck.h

--- deck.h
+++ deck.h
@@ -9,9 +9,9 @@ class Player;
 
 class Deck: public CardCollection<Card> {
 	public:
-	Deck(std::string deckFile, std::shared_ptr<Player> owner, bool shuffle);
-	void shuffle();
-	std::shared_ptr<Card> draw();
+	    Deck(std::string deckFile, std::shared_ptr<Player> owner);
+    	void shuffle();
+    	std::shared_ptr<Card> draw();
 };
 
 #endif
