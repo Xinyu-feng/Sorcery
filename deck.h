4c8d65c8ea74d94fe715aade13ba3d199cb908a0
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-02 03:24:36 -0500

    Update deck.h

--- deck.h
+++ deck.h
@@ -1,8 +1,8 @@
 #ifndef _DECK_H_
 #define _DECK_H_
 
-
 #include "cardcollection.h"
+#include <memory>
 
 class Card;
 class Player;
@@ -11,7 +11,7 @@ class Deck: public CardCollection<Card> {
 	public:
 	    Deck(std::string deckFile, std::shared_ptr<Player> owner);
     	void shuffle();
-    	std::shared_ptr<Card> draw();
+    	void draw(Hand &hand);
 };
 
 #endif
