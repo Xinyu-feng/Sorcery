3d7fcffb49dc25828fa2b21df611e2c3565bbbf0
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-03 01:52:37 -0500

    Update spell.cc

--- spell.cc
+++ spell.cc
@@ -1,8 +1,8 @@
 #include "spell.h"
 #include "state.h"
 
-Spell::Spell(std::string name, std::shared_ptr<Player> owner, int manaCost, string description = "") : 
-    Card{name, owner, manaCost, description} {}
+Spell::Spell(std::string name, int manaCost, string description = "") : 
+    Card{name, manaCost, description} {}
 
 void Spell::runEffect(Subject &board){
 }
