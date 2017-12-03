09cebe1074a36ce25d0f483edf4d95c7492972ee
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-03 00:03:22 -0500

    Update ritual.h

--- ritual.h
+++ ritual.h
@@ -16,6 +16,8 @@ class Ritual: public Card {
     void playCard(Board &b, int target) override;
 
     card_template_t displayCard() override;
+    void addCharges(int i);
+    void addActivateCost(int i);
 };
 
 #endif
