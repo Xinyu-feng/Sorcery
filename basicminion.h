565e5d3c9d86cab4a982b4485ce6c18415942e7c
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-03 01:51:33 -0500

    Update basicminion.h

--- basicminion.h
+++ basicminion.h
@@ -6,7 +6,7 @@
 class BasicMinion: public Minion {
 
 public:
-    BasicMinion(std::string name, std::shared_ptr<Player> owner, int manaCost, int attack, int defence, std::string description = "");
+    BasicMinion(std::string name, int manaCost, int attack, int defence, std::string description = "");
     
     void runEffect(Subject &board) override;
     
