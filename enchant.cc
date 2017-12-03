9c91fecb3e7131a0063c492b57aaff27196b41a6
Aaron Kwan Chuen Leung <akc2leung@uwaterloo.ca>
2017-12-03 16:47:58 -0500

    Update enchant.cc

--- enchant.cc
+++ enchant.cc
@@ -7,13 +7,13 @@
 
 Enchant::Enchant(std::shared_ptr<Minion> m, std::shared_ptr<Enchantment> e):MinionDecorator{m}, e{e}{
     if (e.getName() == "Giant Strength"){
-        m->addStats(2, 2);
+        getMinion()->addStats(2, 2);
     }
     else if (e.getName() == "Enrage"){
-        m->setStats(m.getAttack() * 2, m.getDefense() * 2);
+        getMinion()->setStats(m.getAttack() * 2, m.getDefense() * 2);
     }
     else if (e.getName() == "Magic Fatigue"){
-        m->addAbilityCost(2);
+        getMinion()->addAbilityCost(2);
     }
 }
 
@@ -21,7 +21,7 @@ void Enchant::runEffect(Subject &b){
 
     if (e.getName() != "Silence"){
     
-        m->runEffect(b);
+        getMinion()->runEffect(b);
         
         State s = b->getState();
         
@@ -36,20 +36,20 @@ void Enchant::runEffect(Subject &b){
 
 std::shared_ptr<Enchantment> Enchant::removeEnchant(){
     if (e.getName() == "Giant Strength"){
-        m->lowerStats(2, 2);
+        getMinion()->lowerStats(2, 2);
     }
     else if (e.getName() == "Enrage"){
-        m->setStats(m.getAttack() / 2, m.getDefense() / 2);
+        getMinion()->setStats(getMinion()->getAttack() / 2, getMinion()->getDefense() / 2);
     }
     else if (e.getName() == "Magic Fatigue"){
-        m->lowerAbilityCost(2);
+        getMinion()->lowerAbilityCost(2);
     }
     
-    return m;
+    return getMinion();
 }
 
 card_template_t Enchant::displayCard() {
-	return m->displayCard();
+	return getMinion()->displayCard();
 }
 
 std::vector<card_template_t> Enchant::inspectMinion(){
