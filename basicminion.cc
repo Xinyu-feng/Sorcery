dd56ff69457970f340d70e4fa810ad7f2433aa6e
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-03 01:51:20 -0500

    Update basicminion.cc

--- basicminion.cc
+++ basicminion.cc
@@ -9,8 +9,8 @@
 
 using namespace std;
 
-BasicMinion::BasicMinion(string name, shared_ptr<Player> owner, int manaCost, int attack, int defence, string description = "") : 
-    Minion{name, owner, manaCost, attack, defence, description} {}
+BasicMinion::BasicMinion(string name, int manaCost, int attack, int defence, string description = "") : 
+    Minion{name, manaCost, attack, defence, description} {}
 
 void BasicMinion::runEffect(Subject &board) {
 }
