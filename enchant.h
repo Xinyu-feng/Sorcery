7c73e2c16579a9ddf313575afb566b2aa42240d7
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 07:15:07 -0500

    Update enchant.h

--- enchant.h
+++ enchant.h
@@ -9,14 +9,18 @@
 class Enchant: public MinionDecorator{
     
     std::shared_ptr<Enchantment> e;
-
-    Enchant(std::shared_ptr<Minion> m, std::shared_ptr<Enchantment> e);
+    public:
+        Enchant(std::shared_ptr<AbstractMinion> m, std::shared_ptr<Enchantment> e);
     
-    std::shared_ptr<Minion> removeEnchant();
-    
-    std::vector<card_template_t> inspectMinion() override;
+        std::shared_ptr<AbstractMinion> removeEnchant();
+        int getAttack() override;
+        int getDefence() override;
+        int getActions() override;
+        int getAbilityCost() override;
+        bool active override;
+        std::vector<card_template_t> inspectMinion() override;
 
-    card_template_t displayCard() override;
+        card_template_t displayCard() override;
     
 };
 
