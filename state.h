36b1bcf473e09ba07a01c09b74c95ffdfa0d324e
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 00:48:53 -0500

    Update state.h

--- state.h
+++ state.h
@@ -4,7 +4,7 @@
 enum class Trigger { Begin, Summon, Activate, Leave, End };
 
 struct State {
-    Player player; // player who triggered the notify
+    Player &player; // player who triggered the notify
     Trigger trigger;
     int index = -1; // index of card on board, if trigger is summon/activate/leave
 };
