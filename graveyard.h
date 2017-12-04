aeb5377e68bf459000b377af9685bf706f7afbbc
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 02:34:02 -0500

    Update graveyard.h

--- graveyard.h
+++ graveyard.h
@@ -7,8 +7,8 @@ class Minion;
 
 class Graveyard: public CardCollection<Minion> {
 	public:
-	void moveCardTo(int cardPosition, Hand &h) override;
-	void moveCardTo(int cardPosition, Board &b, int target = -1) override;
+    	void moveCardTo(int cardPosition, Hand &h) override;
+    	void moveCardTo(int cardPosition, Board &b, int target = -1) override;
 };
 
 #endif
