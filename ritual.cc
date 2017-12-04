45f3b40ee8822d0733bd9b88078067c58fdb5276
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 00:33:23 -0500

    Update ritual.cc

--- ritual.cc
+++ ritual.cc
@@ -1,14 +1,32 @@
 #include "ritual.h"
 #include "subject.h"
 #include "player.h"
+#include "state.h"
 
 using namespace std;
 
-Ritual::Ritual(string name, int manaCost, int charges, int activationCost, string description) : 
-    Card{name, manaCost, description}, charges{charges}, activationCost{activationCost} {}
+Ritual::Ritual(std::string name, std::shared_ptr<Player> owner, int manaCost, int charges, int activationCost, string description) : 
+    Card{name, owner, manaCost, description}, charges{charges}, activationCost{activationCost} {}
+
+void Ritual::notify(State s, Player p) {
+    if (active) {
+        p.runEffect(*this, s);
+    }
+    if (s.trigger == Trigger::Begin) {
+        active = false;
+    }
+}
+
+void Ritual::activate() {
+    if (charges < activateCost || active) {
+        // throw...
+    } else {
+        charges -= activateCost;
+        active = true;
+    }
+}
 
 /*
-
 void Ritual::runEffect(Subject &board){
     State s = board.getState();
     if (charges >= activationCost){
@@ -34,12 +52,9 @@ void Ritual::runEffect(Subject &board){
     }
 }*/
 
-void Ritual::playCard(Board &b, int target){
-    b.play(shared_ptr<Ritual>{this});
-}
 
 card_template_t Ritual::displayCard() {
-	return display_ritual(this->getName(), this->getManaCost(), this->activationCost, this->getDescription(), this->charges);
+	return display_ritual(this->getName(), this->getManaCost(), this->activationCost, this->getDescription(), this->charges)
 }
 
 void Ritual::addCharges(int i) { charges += i; }
