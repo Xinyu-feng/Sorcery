3f0843b446e08e52b9a118d462b59a4ba374941d
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 00:30:49 -0500

    Update ritual.h

--- ritual.h
+++ ritual.h
@@ -3,21 +3,24 @@
 
 #include "card.h"
 
+class State;
 
 class Ritual: public Card {
     int charges;
     int activationCost;
+    bool active = false;
     
     public:
-    Ritual(std::string name, int manaCost, int charges, int activationCost, std::string description = "");
+    Ritual(std::string name, std::shared_ptr<Player> owner, int manaCost, int charges, int activationCost, std::string description = "");
     
-    void runEffect(Subject &board);
+    void notify(State s) override;
     
     void playCard(Board &b, int target) override;
 
     card_template_t displayCard() override;
     void addCharges(int i);
     void addActivateCost(int i);
+    void activate();
 };
 
 #endif
