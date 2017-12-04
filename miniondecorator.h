07d2894426caad9b855aea1ce1cd5935871f9e87
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 06:52:30 -0500

    Update creaturedecorator.h

---
+++ miniondecorator.h
@@ -0,0 +1,22 @@
+#ifndef _MINION_DECORATOR_H_
+#define _MINION_DECORATOR_H_
+
+#include "creature.h"
+#include <memory>
+
+class MinionDecorator: public AbstractMinion {
+
+    protected:
+        std::shared_ptr<AbstractMinion> m;
+
+    public:
+        MinionDecorator(std::shared_ptr<AbstractMinion> m);
+    
+        std::shared_ptr<AbstractMinion> getAbstractMinion();
+    
+        virtual ~MinionDecorator() = 0; // do not remove original AbstractMinion
+
+};
+
+#endif
+
