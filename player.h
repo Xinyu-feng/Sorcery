68c6bf5566753d90cd7e7000dfc048d7210993f3
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 01:28:58 -0500

    Update player.h

--- player.h
+++ player.h
@@ -8,17 +8,19 @@
 #include "graveyard.h"
 #include "hand.h"
 
+class State;
+
 class Player {
 	int player;
 	Graveyard graveyard;
 	Hand hand;
 	Board myBoard;
 	Player *otherPlayer;
-	Board *otherBoard;	
-	std::string name;
+	Board *otherBoard;
 	Deck deck;
 	int life = 20;
 	int magic = 3;
+	std::string name;
 	
 	public:
 	Player(int player, std::string name, std::string deckFile, bool shuffle);
@@ -28,23 +30,21 @@ class Player {
 	Board *getBoard();
 
 	void draw(int i = 1);
-	void play(int i, int p, int t = 0);
-	void playTargetCard(std::shared_ptr<Card> c, int t);
+	void play(int i, int p = 0, char t = '\0');
 	void attack(int i, int j = 0);
 	void use(int i, int p, int t);
 	//std::vector<std::string> displayBoard();
 	void discard(int i);
 	std::vector<std::string> displayHand();
 	std::vector<std::string> inspectMinion(int i);
-
-	void destroyMinion(int i);
-	void returnToHand(int i);
 	
-	void deductMagic(int i, bool testing);
+	void deductMagic(int i);
 	int getMagic();
 	
 	void deductLife(int i);
 	int getLife();
+	void notifyApnap(State s);
+	void runEffect(Card &c, State s);
 };
 
 #endif
