27d08272ed611e63ba9e6e0101df9f5c2a8a0f42
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 02:42:26 -0500

    Update card.h

--- card.h
+++ card.h
@@ -1,7 +1,6 @@
 #ifndef _CARD_H_
 #define _CARD_H_
 #include "ascii_graphics.h"
-#include "observer.h"
 #include <string>
 #include <memory>
 
@@ -10,7 +9,7 @@ class Subject;
 class Player;
 class State;
 
-class Card: public Observer{
+class Card {
     std::string name;
     int manaCost;
     std::string description;
