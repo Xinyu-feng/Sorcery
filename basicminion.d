ad78f18f7802a51fb1a7808384a9dd50968fa32b
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 18:16:46 -0500

    running

---
+++ basicminion.d
@@ -0,0 +1,3 @@
+basicminion.o: basicminion.cc basicminion.h minion.h card.h \
+ ascii_graphics.h abstractminion.h subject.h state.h player.h board.h \
+ cardcollection.h hand.h graveyard.h deck.h
---
+++ main.d
@@ -0,0 +1,1 @@
+main.o: main.cc graphicsdisplay.h window.h
