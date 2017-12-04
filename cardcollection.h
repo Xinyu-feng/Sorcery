deeec671d404cee765262ef606738b9710868053
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 05:31:26 -0500

    Update cardcollection.h

--- cardcollection.h
+++ cardcollection.h
@@ -12,24 +12,23 @@ class Hand;
 
 template<typename T> class CardCollection {
 	protected:
-	std::vector <std::shared_ptr<T>> cardList;
-	public:
+	    std::vector <std::shared_ptr<T>> cardList;
+    public:
 	    
-	virtual void moveCardTo(int cardPosition, Hand &h);
-	virtual void moveCardTo(int cardPosition, Graveyard &g);
-	// -1 indicates you want the top card
-	// virtual void moveCardTo(int cardPosition, Deck &d); currently no cards use this
-	virtual void moveCardTo(int cardPosition, Board &b, int target = -1);
+    	virtual void moveCardTo(int cardPosition, Hand &h);
+    	virtual void moveCardTo(int cardPosition, Graveyard &g);
+    	// -1 indicates you want the top card
+    	// virtual void moveCardTo(int cardPosition, Deck &d); currently no cards use this
+    	virtual void moveCardTo(int cardPosition, Board &b, int target = -1);
+      	
+    	void addCard(std::shared_ptr<T> card);
+    	void removeCard(int index);
+    	std::shared_ptr<T> getCard(int i);
 	
-	void addCard(std::shared_ptr<T> card);
-	void removeCard(int index);
-	std::shared_ptr<T> getCard(int i);
-	
-	void setCard(std::shared_ptr<T> card, int index);
-    virtual void discard(int index);
-	
-    int getCardCount();
-    virtual ~CardCollection() = 0;
+    	void setCard(std::shared_ptr<T> card, int index);
+
+        int getCardCount();
+        virtual ~CardCollection() = 0;
 };
 
 #endif
