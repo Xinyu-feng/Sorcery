55bd960d1f7d8cf3df9e66492daf9e704d284d50
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 01:23:13 -0500

    Update ritual.cc

--- ritual.cc
+++ ritual.cc
@@ -5,57 +5,27 @@
 
 using namespace std;
 
-Ritual::Ritual(std::string name, std::shared_ptr<Player> owner, int manaCost, int charges, int activationCost, string description) : 
-    Card{name, owner, manaCost, description}, charges{charges}, activationCost{activationCost} {}
+Ritual::Ritual(std::string name, int manaCost, int charges, int activationCost, string description) : 
+    Card{name, manaCost, description}, charges{charges}, activationCost{activationCost} {}
 
-void Ritual::notify(State s, Player p) {
+void Ritual::notify(State s) {
     if (active) {
-        p.runEffect(getName(), s);
-    }
-    if (s.trigger == Trigger::Begin) {
-        active = false;
+        s.player.runEffect(*this, s);
     }
 }
-
-void Ritual::activate() {
-    if (charges < activateCost || active) {
-        // throw...
-    } else {
-        charges -= activateCost;
-        active = true;
-    }
-}
-
-/*
-void Ritual::runEffect(Subject &board){
-    State s = board.getState();
-    if (charges >= activationCost){
-        if (name == "Dark Ritual"){
-            if (s.owner == owner && s.trigger == Trigger::Begin){
-                owner->addMagic(1);
-                charges -= activationCost;
-            }
-        }
-        else if (name == "Aura of Power"){
-            if (s.owner == owner && s.trigger == Trigger::Summon){
-                s.card->changeAttack(1);
-                s.card->changeDefense(1);
-                charges -= activationCost;
-            }
-        }
-        else if (name == "Standstill"){
-            if (s.trigger == Trigger::Summon){
-                board.sendToGrave(s.position);
-                charges -= activationCost;
-            }
-        }
-    }
-}*/
-
 
 card_template_t Ritual::displayCard() {
 	return display_ritual(this->getName(), this->getManaCost(), this->activationCost, this->getDescription(), this->charges)
 }
 
-void Ritual::addCharges(int i) { charges += i; }
+void Ritual::addCharges(int i) { 
+    charges += i;
+    if (charges < activationCost) {
+        active = false;
+    } else {
+        active = true;
+    }
+}
 void Ritual::addActivateCost(int i) { activationCost += i; }
+
+void Ritual::activate() { addCharges(-activationCost); }
