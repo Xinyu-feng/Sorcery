ebeb5c87373dfc391b9d587de1aed7b83e0b1d65
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 05:21:31 -0500

    Update cardcollection.h

--- cardcollection.h
+++ cardcollection.h
@@ -4,83 +4,32 @@
 #include <memory>
 #include <vector>
 
-
 class Board;
 class Card;
 class Deck;
 class Graveyard;
 class Hand;
 
-class CardCollection {
-    protected:
-    	std::vector <std::shared_ptr<Card>> cardList;
-    public:
-
-
-        // This function moves a card to the collect CardCollection
-        // cardPosition represents the position of the card in the deck
-
-    
-	    virtual void moveCardTo(int cardPosition, Hand &h);
-	    virtual void moveCardTo(int cardPosition, Graveyard &g);
-	   // -1 indicates you want the top card
-	    virtual void moveCardTo(int cardPosition, Deck &d);
-	    virtual void moveCardTo(int cardPosition, Board &b, int target = -1);
+template<typename T> class CardCollection {
+	protected:
+	std::vector <std::shared_ptr<T>> cardList;
+	public:
+	    
+	virtual void moveCardTo(int cardPosition, Hand &h);
+	virtual void moveCardTo(int cardPosition, Graveyard &g);
+	// -1 indicates you want the top card
+	// virtual void moveCardTo(int cardPosition, Deck &d); currently no cards use this
+	virtual void moveCardTo(int cardPosition, Board &b, int target = -1);
 	
-	    void addCard(std::shared_ptr<Card> card);
-	    void removeCard(int index);
-    	std::shared_ptr<Card> getCard(int i);
+	void addCard(std::shared_ptr<T> card);
+	void removeCard(int index);
+	std::shared_ptr<T> getCard(int i);
 	
-    	void setCard(std::shared_ptr<Card> card, int index);
-    	virtual void discard(int index);
+	void setCard(std::shared_ptr<T> card, int index);
+    virtual void discard(int index);
 	
-    	int getCardCount();
-    	virtual ~CardCollection() = 0;
+    int getCardCount();
+    virtual ~CardCollection() = 0;
 };
-
-#include "card.h"
-#include "hand.h"
-#include "graveyard.h"
-#include "deck.h"
-
-template <typename T>
-void CardCollection<T>::addCard(std::shared_ptr<T> card){
-    cardList.emplace_back(card);
-}
-
-template <typename T>
-void CardCollection<T>::removeCard(int index) {
-    cardList.erase(index);
-}
-
-template <typename T>
-void CardCollection<T>::moveCardTo(int cardPosition, Graveyard &g) {
-    cardList.at(cardPosition)->destroy(g);
-    cardList.erase(cardList.begin() + cardPosition);
-}
-
-template <typename T>
-std::shared_ptr<T> CardCollection<T>::getCard(int i){
-    return cardList.at(i);
-}
-
-template <typename T>
-void CardCollection<T>::setCard(std::shared_ptr<T> card, int index){
-    cardList.at(index) = card;
-}
-
-template <typename T>
-int CardCollection<T>::getCardCount(){
-    return cardList.size();
-}
-
-template <typename T>
-CardCollection<T>::~CardCollection() {
-	this->cardList.clear();
-}
-
-
-
-
 
 #endif
