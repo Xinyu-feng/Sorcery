75c4401005a41c4df8d5882fedb1dd51f89627fc
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 05:21:28 -0500

    Update cardcollection.cc

--- cardcollection.cc
+++ cardcollection.cc
@@ -1,1 +1,35 @@
+#include "cardcollection.h"
+#include "board.h"
 
+using namespace std;
+
+void CardCollection<T>::moveCardTo(int cardPosition, Hand &h) {}
+
+void CardCollection<T>::moveCardTo(int cardPosition, Graveyard &g) {}
+
+void CardCollection<T>::moveCardTo(int cardPosition, Board &b, int target = -1) {}
+
+void CardCollection<T>::addCard(std::shared_ptr<T> card){
+    cardList.emplace_back(card);
+}
+
+void CardCollection<T>::removeCard(int index) {
+    cardList.erase(index);
+}
+
+
+shared_ptr<T> CardCollection<T>::getCard(int i){
+    return cardList.at(i);
+}
+
+void CardCollection<T>::setCard(shared_ptr<T> card, int index){
+    cardList.at(index) = card;
+}
+
+int CardCollection<T>::getCardCount(){
+    return cardList.size();
+}
+
+CardCollection<T>::~CardCollection() {
+	this->cardList.clear();
+}
