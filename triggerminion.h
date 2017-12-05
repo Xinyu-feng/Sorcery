9141cdb2c10e285acec154deceb6b6d35a952039
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 21:28:21 -0500

    Add new file

---
+++ triggerminion.h
@@ -0,0 +1,15 @@
+#ifndef _TRIGGERMINION_H_
+#define _TRIGGERMINION_H_
+
+#include "minion.h"
+
+class TriggerMinion: public Minion {
+
+public:
+    TriggerMinion(std::string name, int manaCost, int attack, int defence, std::string description = "");
+
+    card_template_t displayCard() override;
+    std::vector<card_template_t> inspectMinion() override;    
+};
+
+#endif
