49ee61ea142381f1e4c012e4adc418b2aa1e876a
Xinyu Feng <x55feng@uwaterloo.ca>
2017-12-04 21:32:09 -0500

    Update triggerminion.h

--- triggerminion.h
+++ triggerminion.h
@@ -2,14 +2,16 @@
 #define _TRIGGERMINION_H_
 
 #include "minion.h"
+#include "observer.h"
 
-class TriggerMinion: public Minion {
+class TriggerMinion: public Minion, public Observer {
 
 public:
     TriggerMinion(std::string name, int manaCost, int attack, int defence, std::string description = "");
 
     card_template_t displayCard() override;
     std::vector<card_template_t> inspectMinion() override;    
+    void notify(State s) override;
 };
 
 #endif
